8f61ab7151e4da88058ae92e8ba3b51ae951eb31
mvanbroek <m-vanbroek@kellogg.northwestern.edu>
2016-02-07 21:17:55 -0600

    max

--- checklist.js
+++ checklist.js
@@ -3,20 +3,20 @@
 // so this it is prepared to react to user events.
 $(document).ready(function() {
   $(".btn").on("click", addItem);
-  // $(".link-delete").on("click", deleteItem);
+  $(".link-delete").on("click", deleteItem);
   $("ol").on("click", ".link-delete", deleteItem);
 });
 
 // This function prompts the user for a to-do item
 // and then appends it to the existing list of items.
 // It also updates the counter at the top of the screen.
-function addItem() {
-  var text = window.prompt("Something I need");
-  var delete_link = '<a href="#" class="link-delete">(Delete)</a>'
-  // console.log("Yup!");
-  $("ol").append("<li>" + text + " " + delete_link + "</li>");
-  var numItems = $("li").length;
 
+function addItem() {
+  text = window.prompt("Remember to get:");
+  delete_link = '<a href="#" class="link-delete">(Delete)</a>'
+  $("ol").append("<li class='my_checklist'>" + text + " " + delete_link + "</li>");
+
+  var numItems = $(".my_checklist").length;
   // Challenge: Differentiate between 1 item vs. more items
   if (numItems == 1) {
     $(".total").html(numItems + " item");
@@ -24,14 +24,15 @@ function addItem() {
   else {
     $(".total").html(numItems + " items");
   }
+
 }
 
 // This function deletes an item from the list.
 // The "event" parameter holds information on which
 // item the user wishes to remove.
 function deleteItem(event) {
+
   console.info(event);
   // $(event.target).remove();
   $(event.target).parent().fadeOut();
-
 }
